e15067d031f31eef5192265e55372607bbbbb55b
Mateus Antonio <mateus.antonio18@hotmail.com>
2024-09-12 20:18:58 -0300

    add project angular

---
+++ Angular-Loiane/crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  standalone: true,
+  imports: [MatTableModule],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name','category']
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+}
